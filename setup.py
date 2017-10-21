b50b4505c0a4003a220dc6d6fa2f32c45072a84b
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-10-21 19:06:34 +0200

    Upgrade Country template

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'test': [
             'nose',
             'flake8',
-            'openfisca-country-template == 1.3.1',
+            'openfisca-country-template >= 1.4.0a0, <= 1.4.0',
             'openfisca-extension-template == 1.1.1',
             ],
         'tracker': [
